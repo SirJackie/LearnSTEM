1337d76b3697152005d5336a40101789d26c8d4a
lyj00912 <3122742760@qq.com>
2020-08-31 13:37:40 +0800

    created VF2NF() and NF2VF() and p()

--- E003QuadraticFunctionDrawer.py
+++ E003QuadraticFunctionDrawer.py
@@ -1,5 +1,10 @@
 from FrameBuffer import FrameBuffer
 
+
+#
+# Axis Supporting Functions
+#
+
 def DrawAxis(fb, color):
     width = fb.width
     height = fb.height
@@ -22,12 +27,55 @@ def DrawAxis(fb, color):
         fb.DrawPoint((int(width/2-1) - i, 0 + i), color)
         fb.DrawPoint((int(width/2-1) + i, 0 + i), color)
 
+# Convert Point in Axis Into Point in Screen
+def p(x, y, fb):
+    return int((fb.width / 2 - 1) + x), int((fb.height / 2 - 1) - y)
+
+
+#
+# Converting Functions
+#
+
+# Convert Normal Formula to Vertex Formula
+def NF2VF(a, b, c):
+    return a, -1 * b / 2 * a, (4 * a * c - b * b) / 4 * a
 
 # Convert Vertex Formula to Normal Formula
-def vf2nf(a, h, k):
+def VF2NF(a, h, k):
     # return a, b, c
     return a, -2 * a * h, a * h * h + k
 
+
+#
+# Unit Test
+#
+
+def equal(a, b):
+    return abs(float(a) - float(b)) < 0.0005
+
+def UnitTest():
+    result = ""
+
+    # Test1: VF2NF
+    a, b, c = 1, 2, 3
+    print(a, b, c)
+    a, h, k = NF2VF(a, b, c)
+    print(a, h, k)
+    a2, b2, c2 = VF2NF(a, h, k)
+    print(a2, b2, c2)
+    if not (equal(a, a2) and equal(b, b2) and equal(c, c2)):
+        result += "Test1 Rejected\n"
+
+    # Output the Result
+    if result == "":
+        print("Accepted")
+    else:
+        print(result)
+
+
+#
+# Main Function
+#
 
 if __name__ == "__main__":
     fb = FrameBuffer(768, 500, caption="Quadratic Function Drawer")
@@ -35,26 +83,32 @@ if __name__ == "__main__":
 
     DrawAxis(fb, (0, 0, 0))
 
-    mode = input("Please Select Drawing Mode:\n"
-                 "1.Vertex Formula (a, h, k)\n"
-                 "2.Normal Formula (a, b, c)\n")
+    fb.DrawPoint(p(10, 10, fb), (0, 0, 0))
 
-    if mode == "1":
-        a, h, k = map(float, input("Please Input a, h, k\n"
-                                   "(Use Space to Split)\n").split(" "))
+    # mode = input("Please Select Drawing Mode:\n"
+    #              "1.Vertex Formula (a, h, k)\n"
+    #              "2.Normal Formula (a, b, c)\n")
+    #
+    # if mode == "1":
+    #     a, h, k = map(float, input("Please Input a, h, k\n"
+    #                                "(Use Space to Split)\n").split(" "))
+    #
+    #     afunc = lambda a: str(a)
+    #     hfunc = lambda h: "- " + str(h) if h > 0 else "+ " + str(-1 * h)
+    #     kfunc = lambda k: "+ " + str(k) if k > 0 else "- " + str(-1 * k)
+    #
+    #     print("Got Cha! y =", afunc(a), "( x", hfunc(h), ")²", kfunc(k))
+    #
+    # elif mode == "2":
+    #     a, b, c = map(float, input("Please input a, b, c\n"
+    #                                "(Use Space to Split)\n").split(" "))
+    #
+    #     ax2 = lambda a: str(a) + "x²"
+    #     bx = lambda b: "+ " + str(b) + "x" if b > 0 else "- " + str(-1 * b) + "x"
+    #     cfunc = lambda c : "+ " + str(c) if c > 0 else "- " + str(-1 * c)
+    #
+    #     print("Got Cha! y =", ax2(a), bx(b), cfunc(c))
+    #
+    fb.ViewBuffer()
 
-        afunc = lambda a: str(a)
-        hfunc = lambda h: "- " + str(h) if h > 0 else "+ " + str(-1 * h)
-        kfunc = lambda k: "+ " + str(k) if k > 0 else "- " + str(-1 * k)
 
-        print("Got Cha! y =", afunc(a), "( x", hfunc(h), ")²", kfunc(k))
-
-    elif mode == "2":
-        a, b, c = map(float, input("Please input a, b, c\n"
-                                   "(Use Space to Split)\n").split(" "))
-
-        ax2 = lambda a: str(a) + "x²"
-        bx = lambda b: "+ " + str(b) + "x" if b > 0 else "- " + str(-1 * b) + "x"
-        cfunc = lambda c : "+ " + str(c) if c > 0 else "- " + str(-1 * c)
-
-        print("Got Cha! y =", ax2(a), bx(b), cfunc(c))
